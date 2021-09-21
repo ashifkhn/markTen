a53e17e1e35aaa0573f5d06f7d1928c87a9600db
Ashif Khan <ashifkhn02@gmail.com>
2021-09-21 23:40:40 +0530

    DONE

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes=document.querySelectorAll(".no-of-notes")
 
-const availableNotes=[2000, 500, 100, 20, 10, 1]
+const availableNotes=[2000, 500, 100, 20, 10,5, 1]
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
@@ -23,6 +23,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 function calculateChange(amountToBeReturned) {
 
   for(let i=0;i<availableNotes.length;i++){
+    
     const numberOfNotes= Math.trunc(amountToBeReturned/availableNotes[i])
     amountToBeReturned = amountToBeReturned%availableNotes[i]
      noOfNotes[i].innerText=numberOfNotes;
